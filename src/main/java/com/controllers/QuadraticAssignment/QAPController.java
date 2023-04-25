fc7f54b998ed622a99a2e17ccfbc4a7085960ba2
cpolera <25696645+cpolera@users.noreply.github.com>
2023-04-25 18:19:07 -0400

    Spring HATEOAS added
    Implemented HATEOAS for QAP APIs

--- src/main/java/com/controllers/QuadraticAssignment/QAPController.java
+++ src/main/java/com/controllers/QuadraticAssignment/QAPController.java
@@ -1,6 +1,8 @@
 package com.controllers.QuadraticAssignment;
 
 import com.implementations.models.QuadraticAssignment.QAPEntity;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,42 +10,60 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class QAPController {
 
     private final QAPRepository repository;
 
-    QAPController(QAPRepository repository) {
+    private final QAPModelAssembler assembler;
+
+
+    QAPController(QAPRepository repository, QAPModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/qap")
-    List<QAPEntity> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<QAPEntity>> all() {
+        List<EntityModel<QAPEntity>> qapEntities = repository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(qapEntities, linkTo(methodOn(QAPController.class).all()).withSelfRel());
     }
 
     @PostMapping("/qap/create/{filename}")
-    QAPEntity newQAPEntity(@PathVariable String filename) throws FileNotFoundException {
+    EntityModel<QAPEntity> newQAPEntity(@PathVariable String filename) throws FileNotFoundException {
         QAPEntity qapEntity = new QAPEntity("src/main/resources/" + filename + ".txt");
-        return repository.save(qapEntity);
+        repository.save(qapEntity);
+
+        return assembler.toModel(qapEntity);
     }
 
     @GetMapping("/qap/{id}")
-    QAPEntity getQAPEntity(@PathVariable Long id) {
+    EntityModel<QAPEntity> getQAPEntity(@PathVariable Long id) {
 
-        return repository.findById(id)
+        QAPEntity qapEntity = repository.findById(id)
                 .orElseThrow(() -> new QAPEntityNotFoundException(id));
+
+        return assembler.toModel(qapEntity);
     }
 
     @GetMapping("/qap/solve/{id}")
-    QAPEntity solveQAP(@PathVariable Long id) throws Exception {
+    EntityModel<QAPEntity> solveQAP(@PathVariable Long id) throws Exception {
 
         QAPEntity qapEntity = repository.findById(id)
                 .orElseThrow(() -> new QAPEntityNotFoundException(id));
 
         qapEntity.solve();
 
-        return repository.save(qapEntity);
+        repository.save(qapEntity);
+
+        return assembler.toModel(qapEntity);
     }
 }
