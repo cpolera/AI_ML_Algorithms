ccbff1eaeaf8884270b6f6dadc61e9f0ebd06b42
cpolera <25696645+cpolera@users.noreply.github.com>
2023-04-26 16:13:10 -0400

    WebUI Updates
    Added more scaffolding for future pages
    
    Played around with links and determining what data I'll want to display

--- src/main/java/com/controllers/viewers/ViewController.java
+++ src/main/java/com/controllers/viewers/ViewController.java
@@ -5,12 +5,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
+
     @RequestMapping("/view-qap")
     public String viewProducts() {
-        return "viewQAPEntities";
+        return "view-qap";
     }
-    @RequestMapping("/add-qap")
+
+    @RequestMapping("/add-qap") // TODO: remove this separate page and make single QAP page
     public String addProducts() {
         return "add-qap";
+    }
+
+    @RequestMapping("/bayesian")
+    public String viewBayesianClassifier() {
+        return "bayesian";
+    }
+
+    @RequestMapping("/feedforward")
+    public String viewFeedforward() {
+        return "feedforward";
+    }
+
+    @RequestMapping("/markov")
+    public String viewMarkovModel() {
+        return "markov";
+    }
+
+    @RequestMapping("/nearest-neighbor")
+    public String viewNearestNeighbor() {
+        return "nearest-neighbor";
     }
 }
